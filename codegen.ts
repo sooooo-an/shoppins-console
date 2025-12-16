2d4c488affbd1a35eb0d0ac2bfc42fa6122cd686
sooooo-an <devsoo0527@gmail.com>
2025-12-16 21:52:45 +0900

    [fix](init): 타입 에러 수정

--- codegen.ts
+++ codegen.ts
@@ -1,15 +1,15 @@
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://localhost:3000/graphql',
-  documents: ['src/**/*.{ts,tsx,graphql}'],
+  schema: "../api.shoppin.com/src/interface/graphql/schema.gql",
+  documents: ["src/**/*.{ts,tsx,graphql}"],
   generates: {
-    'src/generated/graphql.ts': {
+    "src/generated/graphql.ts": {
       plugins: [
-        'typescript',
-        'typescript-operations',
-        'typescript-react-apollo',
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
       ],
       config: {
         withHooks: true,
@@ -21,4 +21,3 @@ const config: CodegenConfig = {
 };
 
 export default config;
-

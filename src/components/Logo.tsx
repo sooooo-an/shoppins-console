dab93e91f30f069d1bd71dcd5269435963880ff2
sooooo-an <45555359+sooooo-an@users.noreply.github.com>
2025-12-18 12:28:48 +0900

    [feat] logo 변경

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,37 @@
 import { Pin } from "lucide-react";
 
-const Logo = () => {
+type Props = {
+  size?: "small" | "medium" | "large";
+  color?: "white" | "black";
+};
+
+const COLOR_CLASS = {
+  white: "text-white",
+  black: "text-black",
+};
+
+const SIZE_CLASS = {
+  small: "text-base",
+  medium: "text-xl",
+  large: "text-2xl",
+};
+
+const SIZE_LOGO_CLASS = {
+  small: "w-4 h-4",
+  medium: "w-6 h-6",
+  large: "w-8 h-8",
+};
+
+const Logo = ({ size = "medium", color = "white" }: Props) => {
   return (
-    <h1 className="text-white text-2xl font-bold flex items-center gap-2">
+    <h1
+      className={`${COLOR_CLASS[color]} ${SIZE_CLASS[size]} font-bold flex items-center gap-2`}
+    >
       <div className="bg-pink-400 p-2 rounded-xl">
-        <Pin className="w-6 h-6 text-white" fill="currentColor" />
+        <Pin
+          className={`${SIZE_LOGO_CLASS[size]} text-white`}
+          fill="currentColor"
+        />
       </div>
       Shoppins
     </h1>

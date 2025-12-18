1097fbca20e52968a1b00f088811363dc58d0210
sooooo-an <45555359+sooooo-an@users.noreply.github.com>
2025-12-18 12:27:19 +0900

    [feat](init) Header 추가

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,26 @@
+import Logo from "@/components/Logo";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 bg-white shadow-sm">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+        <Logo size="medium" color="black" />
+        <nav className="flex items-center gap-4">
+          <Link href="/console">콘솔로 바로가기</Link>
+          <Link
+            href="/auth/signin"
+            className="bg-teal-600 hover:bg-teal-700 text-white px-4 py-2 rounded-lg transition-colors"
+          >
+            카페24 로그인
+          </Link>
+          {/* <Link>내 상품</Link>
+          <Link>설정</Link>
+          <Link>로그아웃</Link> */}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
